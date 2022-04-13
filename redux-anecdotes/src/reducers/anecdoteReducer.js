409248387c90eed3b79fd15be9d6ea0ab95250e7
ErwannLG <erwannj.legall@gmail.com>
2022-04-13 16:20:58 +0200

    6.3: anecdotes, step1

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,26 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch(action.type) {
+    case 'VOTE':
+      const id = action.data.id
+      const anecdoteToVoteFor = state.find(a => a.id === id)
+      const addVote = {
+        ...anecdoteToVoteFor, votes: anecdoteToVoteFor.votes + 1
+      }
+      return state.map((anecdote) =>
+        anecdote.id !== id ? anecdote : addVote
+      )
+    default:
+      return state
+  }
+}
+
+export const voteFor = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id }
+  }
 }
 
 export default reducer
